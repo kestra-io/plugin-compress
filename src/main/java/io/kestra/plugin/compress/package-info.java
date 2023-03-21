02dd4a7200138957f8f370c658a4790eae32f90f
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:27:22 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/compress/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for compressing and decompressing files.",
+    categories = PluginSubGroup.PluginCategory.TRANSFORMATION
+)
+package io.kestra.plugin.compress;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
